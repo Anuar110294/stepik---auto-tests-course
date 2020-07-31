b78e9c15481cdc9a4ec99f77e7bc5c68c14f37a6
Anuar110294 <69042809+Anuar110294@users.noreply.github.com>
2020-07-31 16:43:41 +0300

    Update 2_4_3.py
    homework from previous module

--- 2_4_3.py
+++ 2_4_3.py
@@ -24,9 +24,9 @@ try:
     button1 = browser.find_element_by_id("solve")
     button1.click()
 
-
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
     # закрываем браузер после всех манипуляций
     browser.quit()
+    
